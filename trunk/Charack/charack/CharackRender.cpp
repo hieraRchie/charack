5580cea9794965d51780d9ca7ae42c9eef964cde
dovyski@gmail.com <dovyski@gmail.com@5dce923f-404c-0410-91af-8b32dad19228>
2008-08-06 13:21:18 +0000

    - Some part of the code (the ugly one) are documented;
    - Camera uses meaningful coordinates now (before this change,we had to decrease y to go up o.O);
    
    - Camera is localized in the middle of the view frustum area.

--- trunk/Charack/charack/CharackRender.cpp
+++ trunk/Charack/charack/CharackRender.cpp
@@ -11,22 +11,41 @@ CharackRender::~CharackRender() {
 
 
 void CharackRender::generateMap(void) {
-	int mapX, mapZ, x, z;
+	int aMapX, aMapZ, x, z;
 	int aXNow = (int)getCamera()->getPositionX();
 	int aZNow = (int)getCamera()->getPositionZ();
 
 	system("cls");
-	printf("(z,y,z) = (%d,%d,%d), (rotx, roty) = (%d, %d)\n", (int)getCamera()->getPositionX(), (int)getCamera()->getPositionY(), (int)getCamera()->getPositionZ(), getCamera()->getRotationX(), getCamera()->getRotationY());
+	printf("(x,y,z) = (%d,%d,%d), (rotx, roty) = (%d, %d)\n", (int)getCamera()->getPositionX(), (int)getCamera()->getPositionY(), (int)getCamera()->getPositionZ(), getCamera()->getRotationX(), getCamera()->getRotationY());
 
-	mapX = abs(aXNow) - CK_VIEW_FRUSTUM;
-	mapZ = abs(aZNow) - CK_VIEW_FRUSTUM;
+	// What we have here is this:
+	//
+	// aXNow and aZNow hold the value of our current position in the Charack world. To populate
+	// mMap with data, we will collect all points localized within the square centered at (aXNow, aZNow) (in the figure
+	// below, the square has A, B, C and D as its corners).
+	//
+	// The size of the square is determinated by CK_VIEW_FRUSTUM. We will iterate from A to B (X axis) and, for each step,
+	// we will iterare from A to D (or B to C) (Z axis) collecting points.
+	//
+	// Each of these collected points will be stored at mMap[x][y]. x and y will just be used to index
+	// the mMap array, they have no relation with the world coordinates.
+	//
+	// -------------D------------|------------C    -
+	// |            |            |            |    |
+	// |            |            |            |    |
+	// |------------|-------(aXNow,aZNow)-----| -- | CK_VIEW_FRUSTUM
+	// |            |            |            |    |
+	// |            |            |            |    |
+	// -------------A------------|------------B    -
+	//        (aMapX, aMapZ)			|
+	//									|
+	//							 |------------|
+	//							 CK_VIEW_FRUSTUM/2
 
-	for(x = 0; x < CK_VIEW_FRUSTUM; x++){ 
-		for(mapZ = 0, z = 0; z < CK_VIEW_FRUSTUM; z++){ 
-			mMap[x][z] = Vector3(x, getHeight(mapX, mapZ), z);
-			mapZ++;
+	for(aMapX = abs(aXNow) - CK_VIEW_FRUSTUM/2, x = 0; x < CK_VIEW_FRUSTUM; x++, aMapX++){ 
+		for(aMapZ = abs(aZNow) - CK_VIEW_FRUSTUM/2, z = 0; z < CK_VIEW_FRUSTUM; z++, aMapZ++){ 
+			mMap[x][z] = Vector3(x, getHeight(aMapX, aMapZ), z);
 		}
-		mapX++;
 	}
 }
 
@@ -36,7 +55,8 @@ void CharackRender::displayMap(void) {
 
 	glRotatef(getCamera()->getRotationY(), 0,1,0);
 	glRotatef(getCamera()->getRotationX(), 1,0,0);
-	glTranslatef(-CK_VIEW_FRUSTUM/2, getCamera()->getPosition()->y, -CK_VIEW_FRUSTUM/2);
+
+	glTranslatef(-CK_VIEW_FRUSTUM/2, -getCamera()->getPosition()->y, -CK_VIEW_FRUSTUM/2);
 
 	generateMap();
 
@@ -71,7 +91,7 @@ void CharackRender::displayMap(void) {
 }
 
 float CharackRender::getHeight(float theX, float theZ) {
-	return sin(abs(theX)/(CK_MAX_WIDTH/20)) * 20 + cos(abs(theZ)/(CK_MAX_WIDTH/40)) * 10;
+	return sin(abs(theX)/(CK_MAX_WIDTH/20)) * 20 + cos(abs(theZ)/(CK_MAX_WIDTH/30)) * 10;
 	//return getMathCollectionX()->getValue(abs(theX)/CK_MAX_WIDTH) + getMathCollectionZ()->getValue(abs(theZ)/CK_MAX_WIDTH);
 }
 
