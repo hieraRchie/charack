e2474f0913ec41bd832c1ad8e8867ae2e4e99eb3
dovyski@gmail.com <dovyski@gmail.com@5dce923f-404c-0410-91af-8b32dad19228>
2008-08-22 14:19:06 +0000

    - The code is not so organized, but I'm commiting because the terrain really looks like a terrain ^^ Thank you so much, Ken Perlin!

--- trunk/Charack/height.cpp
+++ trunk/Charack/height.cpp
@@ -1,9 +1,17 @@
 #include "height.h"
 
-float fx1(float a) { return sin(a/(CK_MAX_WIDTH/20)); }
+Perlin gPerlinNoise(4, 4, 1, 94);
+
+float fx1(float a) {
+	return gPerlinNoise.Get(a/(CK_MAX_WIDTH/2000), 0.22);
+	//return sin(a/(CK_MAX_WIDTH/20));
+}
 float fx2(float a) { return sin(a/(CK_MAX_WIDTH/20000)); }
 float fx3(float a) { return cos(a/(CK_MAX_WIDTH/200000)); }
 
-float fz1(float a) { return cos(a/(CK_MAX_WIDTH/20)); }
+float fz1(float a) {
+	return gPerlinNoise.Get(a/(CK_MAX_WIDTH/2000), 0.22);
+	//return cos(a/(CK_MAX_WIDTH/20));
+}
 float fz2(float a) { return cos(a/(CK_MAX_WIDTH/20000)); }
 float fz3(float a) { return cos(a/(CK_MAX_WIDTH/200000)); }
