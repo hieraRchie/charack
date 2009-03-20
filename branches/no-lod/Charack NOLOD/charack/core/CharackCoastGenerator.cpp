3022c65e89c7796a2f35ee1904ebff1523f3fec7
dovyski@gmail.com <dovyski@gmail.com@5dce923f-404c-0410-91af-8b32dad19228>
2009-03-20 14:37:03 +0000

    - Free BSD License for all files; - Cleaning up the code a little bit;

--- branches/no-lod/Charack NOLOD/charack/core/CharackCoastGenerator.cpp
+++ branches/no-lod/Charack NOLOD/charack/core/CharackCoastGenerator.cpp
@@ -1,3 +1,30 @@
+/**
+ * Copyright (c) 2009, Fernando Bevilacqua
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Universidade Federal de Santa Maria nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Fernando Bevilacqua ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL Fernando Bevilacqua BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include "CharackCoastGenerator.h"
 
 CharackCoastGenerator::CharackCoastGenerator() {
@@ -37,17 +64,13 @@ void CharackCoastGenerator::disturbStraightCoastLines(unsigned char *theHeightDa
 			aDistanceUp		= distanceFromWater(theMapGenerator, xObserver, zObserver, theSample, MOVE_UP);
 			aDistanceDown	= distanceFromWater(theMapGenerator, xObserver, zObserver, theSample, MOVE_DOWN);
 
-			aTotalDistance	= aDistanceRight + aDistanceLeft + aDistanceUp + aDistanceDown;
+			aTotalDistance	= (float)(aDistanceRight + aDistanceLeft + aDistanceUp + aDistanceDown);
 			aBeachHeight	= floor((aTotalDistance/CK_COAST_MAX_SEA_DISTANCE) * getMaxBeachHeight());
 
 			// If we are far away from the coast, we use the height information of the land portion.
 			// If we are close to the coast, we use the beach height, so we can produce a smooth transition
 			// between land and water.
-			//aHeight = (unsigned char)(aTotalDistance < CK_COAST_MAX_SEA_DISTANCE ? aBeachHeight : theHeightData[i]);
-			if(theHeightData[i] > 5) {
-				printf("%u, ", theHeightData[i]);
-			}
-			aHeight = theHeightData[i];
+			aHeight = (unsigned char)(aTotalDistance < CK_COAST_MAX_SEA_DISTANCE ? aBeachHeight : theHeightData[i]);
 			
 			// Avoid negative heights... 
 			aHeight = aHeight < 0 ? 0 : aHeight;
--- branches/no-lod/Charack NOLOD/charack/core/config.h
+++ branches/no-lod/Charack NOLOD/charack/core/config.h
@@ -1,3 +1,30 @@
+/**
+ * Copyright (c) 2009, Fernando Bevilacqua
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Universidade Federal de Santa Maria nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Fernando Bevilacqua ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL Fernando Bevilacqua BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #ifndef __CHARACK_CONFIG_H_
 #define __CHARACK_CONFIG_H_
 
@@ -9,14 +36,14 @@
 #define CK_SEA_LEVEL					400
 #define CK_SEA_BOTTON					0
 
-#define CK_COAST_MAX_STEP				100
+#define CK_COAST_MAX_STEP				25
 #define CK_COAST_MAX_SEA_DISTANCE		(CK_COAST_MAX_STEP * 4)
 #define CK_COAST_VARIATION				0.20
-#define CK_COAST_BEACH_HEIGHT			110
+#define CK_COAST_BEACH_HEIGHT			4
 #define CK_COAST_MANGLE_HEIGHT			0
 
 // Max world width/	height
-#define CK_MAX_WIDTH					3000000.0
+#define CK_MAX_WIDTH					3000000.0f
 
 // Useful macros
 #define CK_DEG2RAD(X)					((PI*(X))/180)
